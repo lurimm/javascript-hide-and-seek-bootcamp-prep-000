85715f041cc6c34672ec29dd04d3445940967508
Luis Rincon <rincon_92@hotmail.com>
2017-02-05 22:41:48 +0000

    Done.

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ function nestedTarget() {
 }
 
 function increasedByRank(n) {
-  var list = document.getElementById('app').querySelectorAll('ul.ranked-list li')
+  var list = document.getElementById('app').querySelectorAll('ul.ranked-list li');
   for (var i = 0, l = list.length; i < l; i++) {
-    parseInt(list[i].innerHTML) = i + n;
+    parseInt(list[i].innerHTML) + n = list[i].innerHTML;
   }
 }
 
 function deepestChild() {
-
+  var grandNode = document.getElementById('grand-node').querySelectorAll('div');
+  return grandNode[3];
 }
